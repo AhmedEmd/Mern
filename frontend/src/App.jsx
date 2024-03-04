d5e96848472d07aabc03e8ea6789becd4922570d
AhmedEmd <ah.em6465@gmail.com>
2024-03-04 05:24:31 -0800

    Commiting react pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import {Routes , Route} from 'react-router-dom'
+import CreateBook from './pages/CreateBooks';
+import DeleteBooks from './pages/DeleteBooks';
+import EditBook from './pages/EditBook';
+import Home from './pages/Home';
+import ShowBooks from './pages/ShowBooks';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>
-      
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={< Home/>} />
+      <Route path="/books/create" element={< CreateBook/>} />
+      <Route path="/books/details/:id" element={< ShowBooks/>} />
+      <Route path="/books/edit/:id" element={< EditBook/>} />
+      <Route path="/books/delete/:id" element={< DeleteBooks/>} />
+    </Routes>
+  );
+};
+
 
 export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import Spinner from '../components/spinner';
+import { Link } from 'react-router-dom';
+import {AiOutlineEdit} from 'react-icons/ai'
+import {BsInfoCircle} from 'react-icons/bs'
+import {MdOutlineAddBox , MdOutlineDelete} from 'react-icons/md'
+
+const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
